cf69e4fba2c2338705fd4b44a95228a405486f86
Gerard <gerard.hernandez@gmail.com>
2026-01-17 20:40:17 +0000

    Added a new component Navbar.svelte, added lucide as sourcce of icons.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<!-- <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 
@@ -6,4 +6,15 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+{@render children()} -->
+<script>
+  import "./layout.css";
+  import Navbar from "$lib/components/nav/Navbar.svelte";
+  //import favicon from "$lib/assets/favicon.svg";
+
+  //	let { children } = $props();
+</script>
+
+<Navbar />
+
+<slot />
